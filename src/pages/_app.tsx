d02f92f4d164e9d00489d7a447dd445d6ce4f211
qyubitmetaverse <118708829+onezeno717@users.noreply.github.com>
2022-12-31 16:30:27 +0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../../styles/globals.css";
 import { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
+
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout: (page: ReactElement) => ReactNode;
 };
