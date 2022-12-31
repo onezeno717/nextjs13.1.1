4c9257f5923c7f3c5d8dfefd1e131ca72c4a9ebb
qyubitmetaverse <118708829+onezeno717@users.noreply.github.com>
2022-12-31 09:58:38 +0700

    install tailwindcss, framer motion, prettier

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import "../../styles/globals.css";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return getLayout(<Component {...pageProps} />);
+}
